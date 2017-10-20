9cefd97a67e953fe87f4e4328ffe6c2594c8b9ca
secr3t <kkandoli11@gmail.com>
2017-10-20 23:05:40 +0900

    find a way that I had to debug

--- src/algorithm/ReduplicativePermutation.java
+++ src/algorithm/ReduplicativePermutation.java
@@ -46,5 +46,15 @@ public class ReduplicativePermutation {
     public ArrayList<String> getResults() {
     	return result;
     }
+    /*
+     *  배열의 길이를 초과하는 가짓수에 대한 해결법 
+     * 	u, o 일 경우 초과하는 가짓수에 대해서 같은 String 하나를 더 추가한 배열을 선언하여 
+     * ex) 2개중에 3개를 중복하여 순열을 만들경우
+     * 	String[] uo = {"언", "옵", "옵"}; 
+     * 에서 uo를 갖고 perm(2,3)을 진행한다. 그것의 결과를
+     * hashset에 담아서 재출력하면 중복된것들이 제거되고 2PI3의 결과값이 나오게 된다.
+     * 굉장히 위험하고 메모리 누수가 걱정되며 알고리즘 적으로 굉장히 비효율적이지만
+     * 현재 상황에서 간단하게 해결할 수 있는 방법 중 하나이다.
+    */
 }
  
